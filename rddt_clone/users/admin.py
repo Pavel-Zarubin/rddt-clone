af148aa2238f31f3591562eaac5700a5f010b5f6
Pavel-Zarubin <zarubinpasha2@gmail.com>
2019-04-11 16:03:12 +0300

    Добавлено действие в админ панели.

--- rddt_clone/users/admin.py
+++ rddt_clone/users/admin.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
 from .models import User
 
+
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ['id', 'username', 'gender', 'email', 'inn']
     ordering = ['username']
-    actions = ['fem_invasion']
+    actions = ['fem_attack']
 
-    def fem_invasion(self, request, queryset):
-        queryset.update(gender='female')
-    fem_invasion.short_description = 'Make user a female'
+    def fem_attack(self, request, queryset):
+        female = User.objects.filter(gender='female').count()
+        male = User.objects.filter(gender='male').count()
 
+        if female < male:
+            male = User.objects.filter(gender='male')[:1]
+            User.objects.filter(id__in=male).update(gender='female')
+
+        else:
+            return False
