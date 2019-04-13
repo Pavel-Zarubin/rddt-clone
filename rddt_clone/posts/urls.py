bd0e4100b8087407245d91b1080fa340dec2c05c
Pavel-Zarubin <zarubinpasha2@gmail.com>
2019-04-13 11:50:28 +0300

    Добавлена таблица постов с фильтром по полю 'Author'.

--- rddt_clone/posts/urls.py
+++ rddt_clone/posts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView, CreatePostComment
-
+from .views import (PostListView, PostDetailView,
+                    PostCreateView, PostUpdateView,
+                    PostDeleteView, UserPostListView,
+                    CreatePostComment,)
+from django_filters.views import FilterView
+from .filters import PostFilter
 
 urlpatterns = [
-
     path('', PostListView.as_view(), name='posts_list_All'),
     path('user/<str:username>', UserPostListView.as_view(), name='user_posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
@@ -11,4 +14,6 @@ urlpatterns = [
     path('submit/', PostCreateView.as_view(), name='post_create'),
     path('post/<int:pk>/update', PostUpdateView.as_view(), name='post_update'),
     path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post_delete'),
+    path('table', FilterView.as_view(filterset_class=PostFilter), name='post_filter'),
+
 ]
